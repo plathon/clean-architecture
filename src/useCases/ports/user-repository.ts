e379a4c87b3eaefa7e6a9d81c3e99063dd917131
= <=>
2021-01-19 01:17:33 -0300

    feat: register user use case

--- src/useCases/ports/user-repository.ts
+++ src/useCases/ports/user-repository.ts
@@ -1,3 +1,6 @@
+import { UserData } from '@entities/user/user-data'
+
 export interface UserRepository {
   exists(email: string): Promise<boolean>
+  create(user: UserData): Promise<void>
 }
--- src/useCases/register-user.ts
+++ src/useCases/register-user.ts
@@ -1,19 +1,26 @@
 /* eslint-disable no-unused-vars */
 import { InvalidEmailError } from '@entities/user/errors/InvalidEmailError'
 import { InvalidNameError } from '@entities/user/errors/InvalidNameError'
-import { User, UserData } from '@entities/user/user'
-import { Either, left } from '@shared/either'
+import { User } from '@entities/user/user'
+import { UserData } from '@entities/user/user-data'
+import { Either, left, right } from '@shared/either'
 import { UserRepository } from './ports/user-repository'
 
 export class RegisterUser {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(userData: UserData): Promise<void> {
+  async execute(
+    userData: UserData
+  ): Promise<Either<InvalidNameError | InvalidEmailError, User>> {
     const userOrError = User.create(userData)
     if (userOrError.isLeft()) {
-      left(userOrError.value)
+      return left(userOrError.value)
     }
     const user = userOrError.value
-    const userAlreadyExists = await this.userRepository.exists('mock')
+    const userAlreadyExists = await this.userRepository.exists(user.email)
+    if (!userAlreadyExists) {
+      await this.userRepository.create(userData)
+    }
+    return right(user)
   }
 }
