f1dc6b2d132b71771e2b7a5d487ec3511e61f094
= <=>
2021-01-28 11:49:34 -0300

    test: unit testing user entity

--- src/entities/user/errors/invalid-email-error.ts
+++ src/entities/user/errors/invalid-email-error.ts
@@ -1,6 +1,4 @@
-import { UserError } from './user-error'
-
-export class InvalidEmailError extends Error implements UserError {
+export class InvalidEmailError extends Error {
   constructor(email: string) {
     super(`the email: ${email} is not valid`)
     this.name = email
